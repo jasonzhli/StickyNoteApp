f8bd7287ee3178beb5a009b15e125da1a17134f1
Jason <jzl.java@gmail.com>
2019-07-12 01:52:24 -0700

    finished project and added README content

--- README.md
+++ README.md
@@ -16,34 +16,22 @@ Create Read Update and Delete framework using JS
 
 create, update/modify data, and delete data, and view that data
 
-ideas:
+-----
 
-Most likely to do number 2.
+This web app is a sticky note wall or dashboard, similar to that of google keep or tumblr.
 
-1) have some type of wishlist where you can add,update, and remove items.
-basiq reqs: 
--price
--item
--picture
-additional reqs:
--add an amazon link and it will search that webpage for price, picture, amazon prime or not?
--have others able to click and search for the item in google shopping
--have price update live from a good site
+ ## It has the following basic functionalities:
+- Add single sticky note
+	- includes title, note body, and color
+- Delete single sticky note
+- Edit single sticky note
+- Clear all notes on dashboard/wall
 
-2) to do list/sticky note page/calendar that can add update and remove items
-basic reqs:
--add sticky note
--add items on sticky note
--cross out sticky note
--similar to google notes/google keep
--this would be so cool!!
--make it colorful
-advanced:
--make it like a calendar
--add pictures in notes
+ ## and the following cool addons:
+- Delete and edit clickable icon on each sticky note, that fade in on note hover
+- time created at bottom of sticky note
+- reactive to browser sizing for columns
 
-
-3) online white elephant where you have a list and add stuff?
--list of items and you can pick and choose when you wnat to start the picking and choosing
--this would be much harder
+ ## Did not get time to complete:
+ - Adding pictures to dashboard
 
--- app/app.js
+++ app/app.js
@@ -12,7 +12,8 @@ var createItem = function(key, value) {
 }
 
 var updateItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+  var stringifyValue = JSON.stringify(value);
+  return window.localStorage.setItem(key,stringifyValue);
 }
 
 var deleteItem = function(key) {
@@ -23,20 +24,6 @@ var clearDatabase = function() {
   return window.localStorage.clear();
 }
 
-// var showDatabaseContents = function() {
-//   $('tbody').html('');
-
-//   for (var i = 0; i < window.localStorage.length; i++) {
-//     var key = window.localStorage.key(i);
-//     $('tbody').append(`<tr><td>${key}</td><td>${window.localStorage.getItem(key)}</td></tr>`)
-//   }
-// }
-
-// div class="note h-auto rounded-lg p-2 m-1 bg-info text-white" style="width: 250px"
-        // <button type="button" class="delete-note btn-xs btn-danger m-1 mr-4"><small>Delete</small></button>
-        // <button type="button" class="btn-xs btn-primary m-1" data-toggle="modal" data-target="#exampleModal"><small>Edit</small></button>
-
-        // after note-card: style="max-width: 18rem; min-width: 10rem"
 var showNotes = function() {
   $('.card-columns').html('');
 
@@ -45,8 +32,26 @@ var showNotes = function() {
     var value = JSON.parse(window.localStorage.getItem(key))[0];
     var date = timeago.format(JSON.parse(window.localStorage.getItem(key))[1]);
     var color = JSON.parse(window.localStorage.getItem(key))[2];
+    var colorName;
+    if (color === '#F8F9FA') {
+      colorName = 'White';
+    } else if (color === '#F28B82') {
+      colorName = 'Red';
+    } else if (color === '#CBF0F8') {
+      colorName = 'Blue';
+    } else if (color === '#A7FFEB') {
+      colorName = 'Teal';
+    } else if (color === '#FFF475') {
+      colorName = 'Yellow';
+    } else if (color === '#E6CAA8') {
+      colorName = 'Brown';
+    } else if (color === '#E8EAED') {
+      colorName = 'Grey';
+    }
+
     var element = 
-    `<div class="card mb-3 note-card" style="background-color: ${color}">
+    `<div>
+    <div class="card mb-3 note-card" style="background-color: ${color}">
       <div class="card-body">
         <h5 class="card-title note-title">${key}</h5>
         <div class="note-body">
@@ -56,11 +61,58 @@ var showNotes = function() {
       <div class="row mt-1 wrapper">
         <small class="text-muted ml-4 mb-3">&nbsp;&nbsp;&nbsp;created ${date}</small>
         <div class="footer-buttons">
-          <i class="m-1 p-1 edit-delete-buttons" data-feather="edit-2"></i>
+          <i class="m-1 p-1 edit-delete-buttons" data-feather="edit-2" data-toggle="modal" data-target="#editmodal${i}"></i>
           <i class="delete-note m-1 mr-4 p-1 edit-delete-buttons" data-feather="trash-2"></i>
         </div>
       </div>
     </div>
+
+    <div class="modal fade" id="editmodal${i}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit Note</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="title-name" class="col-form-label">Title:</label>
+                <input type="text" class="form-control key2" id="title-name" placeholder="Title" value="${key}">
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label">Note:</label>
+                <textarea class="form-control value2" id="message-text" placeholder="Write your note here...">${value}</textarea>
+              </div>
+              <label class="col-form-label">Color:</label>
+              <div class="dropdown">
+                <button class="btn-sm m-2 btn-secondary dropdown-toggle drop-down" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  ${colorName}
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                  <button class="dropdown-item white-color" type="button">White</button>
+                  <button class="dropdown-item red-color" type="button">Red</button>
+                  <button class="dropdown-item blue-color" type="button">Blue</button>
+                  <button class="dropdown-item teal-color" type="button">Teal</button>
+                  <button class="dropdown-item yellow-color" type="button">Yellow</button>
+                  <button class="dropdown-item brown-color" type="button">Brown</button>
+                  <button class="dropdown-item grey-color" type="button">Grey</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-primary edit" data-dismiss="modal">Save Changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+
     <script>
       feather.replace()
     </script>`;
@@ -87,7 +139,6 @@ var resetInputs = function() {
 }
 
 $(document).ready(function() {
-  // showDatabaseContents();
   showNotes();
   
   $(".card-columns")
@@ -96,36 +147,29 @@ $(document).ready(function() {
     $(this).find(".edit-delete-buttons").fadeIn();
   })
   .on("mouseleave", ".note-card", function() {
-    // $(this).find(".footer-buttons").css("display","none");
     $(this).find(".edit-delete-buttons").fadeOut(200);
   });
 
-  // $(document).on('mouseenter', '.note-card', function () {
-  //       $(this).find(":icon").show();
-  //   }).on('mouseleave', '.note-card', function () {
-  //       $(this).find(":icon").hide();
-  //   });
 
   $(".dropdown-menu").on('click', '.dropdown-item', function(){
-      // console.log($("#dropdownMenu:first-child").text());
+    $("#dropdownMenu:first-child").text($(this).text());
+    $("#dropdownMenu:first-child").val($(this).text());
+    $("#dropdownMenu2:first-child").text($(this).text());
+    $("#dropdownMenu2:first-child").val($(this).text()); 
+  });
 
-      $("#dropdownMenu:first-child").text($(this).text());
-      $("#dropdownMenu:first-child").val($(this).text());
 
-   });
-
-  $('.delete-note').on('click',function() {
-    // console.log(this.parentElement.getElementsByTagName('strong')[0].innerHTML);
-    console.log(this);
-    console.log(this.parentElement.parentElement);
-    // location.reload(true);
-    // showDatabaseContents();
-    // showNotes();
+  $('.card-columns').on('click', '.delete-note', function() {
+    if (confirm('Delete this note?')) {
+      deleteItem(this.parentElement.parentElement.parentElement.getElementsByClassName('note-title')[0].innerHTML);
+      showNotes();
+    }
   })
 
-  $('.create').on('click', function() {
+  $('.card-columns').on('click', '.edit', function() {
     var date = new Date();
-    var color = $("#dropdownMenu:first-child").text();
+    var color = this.parentElement.parentElement.getElementsByClassName('drop-down')[0].innerHTML;
+    color = color.trim();
     if (color === 'White') {
       color = '#F8F9FA';
     } else if (color === 'Red') {
@@ -140,20 +184,51 @@ $(document).ready(function() {
       color = '#E6CAA8';
     } else if (color === 'Grey') {
       color = '#E8EAED';
-    }
+    };
+
+    var oldKey = this.parentElement.parentElement.parentElement.parentElement.parentElement.getElementsByClassName('note-title')[0].innerHTML;
+    var keyInput = this.parentElement.parentElement.parentElement.getElementsByClassName('key2')[0].value;
+    var valueText = this.parentElement.parentElement.parentElement.getElementsByClassName('value2')[0].value;
+    var valueInput = [valueText,date,color];  
+ 
+    if (keyInput === oldKey) {
+      updateItem(keyInput, valueInput);
+      showNotes();
+      document.location.reload()
+      
+    } else if (keyInput !== oldKey) {
+      deleteItem(oldKey);
+      createItem(keyInput, valueInput);
+      showNotes();
+      document.location.reload()
+    } 
+
+
+  })
+
+  $('.create').on('click', function() {
+    var date = new Date();
+    var color = $("#dropdownMenu:first-child").text();
+    console.log(color);
+    color = color.trim();
+    if (color === 'White') {
+      color = '#F8F9FA';
+    } else if (color === 'Red') {
+      color = '#F28B82';
+    } else if (color === 'Blue') {
+      color = '#CBF0F8';
+    } else if (color === 'Teal') {
+      color = '#A7FFEB';
+    } else if (color === 'Yellow') {
+      color = '#FFF475';
+    } else if (color === 'Brown') {
+      color = '#E6CAA8';
+    } else if (color === 'Grey') {
+      color = '#E8EAED';
+    };
 
     if (getKeyInput() !== '' && getValueInput() !== '') {
-      // if (keyExists(getKeyInput())) {
-      //   if(confirm('key already exists in database, do you want to update instead?')) {
-      //     updateItem(getKeyInput(), getValueInput());
-      //     showDatabaseContents();
-      //   }
-      //   // createItem(keyID);
-      //   showNotes();
-      //   resetInputs();
-      // }
-      var valueInput = [getValueInput(),date,color]; // try
-      console.log(valueInput);
+      var valueInput = [getValueInput(),date,color]; 
       createItem(getKeyInput(),valueInput);
       showNotes();
       resetInputs();
@@ -161,28 +236,13 @@ $(document).ready(function() {
     else {
       alert('key and value must not be blank');
     }
-  })
-// });
-
-  $('.update').on('click', function() {
-    if (getKeyInput() !== '' && getValueInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        updateItem(getKeyInput(), getValueInput());
-        // showDatabaseContents();
-        resetInputs();
-      } else {
-        alert('key does not exist in database');
-      }
-    } else {
-      alert('key and value must not be blank');
-    }
   });
+
 
   $('.delete').on('click', function() {
      if (getKeyInput() !== '') {
       if (keyExists(getKeyInput())) {
         deleteItem(getKeyInput());
-        // showDatabaseContents();
         showNotes();
         resetInputs();
       } else {
@@ -193,14 +253,10 @@ $(document).ready(function() {
     }
   });
 
-  $('.reset').on('click', function() {
-    resetInputs();
-  })
 
   $('.clear').on('click', function() {
-    if (confirm('WARNING: Are you sure you want to clear the database? \n                THIS ACTION CANNOT BE UNDONE')) {
+    if (confirm('WARNING: Are you sure you want to clear your wall? \n                THIS ACTION CANNOT BE UNDONE')) {
       clearDatabase();
-      // showDatabaseContents();
       showNotes();
     }
   })
